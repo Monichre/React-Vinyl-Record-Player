8e0798a94d585d44c45d224446f4ef766245552b
Liam Ellis <liamhellis@gmail.com>
2017-12-02 21:58:52 -0500

    flux set up

--- src/Dispatch/Dispatcher.js
+++ src/Dispatch/Dispatcher.js
@@ -1,3 +1,8 @@
 import { Dispatcher } from 'flux'
 import { getStore } from '../Actions/Actions'
 
+const Dispatcher = new Dispatcher()
+
+Dispatcher.register((payload) => {
+    let action = payload.action
+})
